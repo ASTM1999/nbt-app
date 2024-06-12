161fde914a23abdd5ff8803249ec0fb5f284e614
ASTM1999 <anusornsriprom1999@gmail.com>
2024-06-12 22:39:03 +0700

    v1

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,23 +6,16 @@ import { MongooseModule } from '@nestjs/mongoose';
 import { ProjectModule } from './project/project.module';
 import { TaskModule } from './task/task.module';
 import { CommentModule } from './comment/comment.module';
-import { ConfigModule, ConfigService } from '@nestjs/config';
+import { ConfigModule } from '@nestjs/config';
 import { EncryptionService } from './utils/encryption.service';
-import configuration from './common/env';
+
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       isGlobal: true,
     }),
-
-    MongooseModule.forRootAsync({
-      imports: [ConfigModule],
-      useFactory: async (configuration: ConfigService) => ({
-        uri: configuration.get<string>('DATABASE_URL')
-      }),
-      inject: [ConfigService]
-    }),
+    MongooseModule.forRoot(process.env.DATABASE_URL),
     UsersModule,
     ProjectModule,
     TaskModule,
--- src/common/env.ts
+++
@@ -1,9 +0,0 @@
-// env.ts
-export default () => ({
-  // Add your own properties here however you'd like
-  // port: parseInt(process.env.PORT, 10) || 3000,
-  database: {
-    host: process.env.DATABASE_URL,
-    // port: parseInt(process.env.DATABASE_URL, 10) || 5432
-  }
-});
