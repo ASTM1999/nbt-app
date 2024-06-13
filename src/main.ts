85d9c15dae18a346a2a594ca22582e0f91661600
ASTM1999 <anusornsriprom1999@gmail.com>
2024-06-13 13:30:57 +0700

    update ipwhitelistmiddleware

--- src/main.ts
+++ src/main.ts
@@ -1,18 +1,22 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ConfigService } from '@nestjs/config';
 
-const port = process.env.PORT || 3000;
+// const port = process.env.PORT || 3000;
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const configService = app.get(ConfigService);
+
+  const allowedOrigins = configService.get<string>('ALLOWED_ORIGINS')?.split(',') || [];
+
   app.enableCors({
-    origin: [
-      'http://localhost:5173',
-      'https://toolkithub282.web.app'
-    ],
-    // credentials: true,
-    allowedHeaders: ['Content-Type', 'Authorization'], 
+    origin: allowedOrigins,
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    credentials: true,
   });
+
+  const port = configService.get<number>('PORT') || 3000;
   await app.listen(port, "0.0.0.0");
 }
 bootstrap();
